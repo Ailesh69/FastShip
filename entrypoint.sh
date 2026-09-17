a2d7065d7da1b385cea4261ff29e3f49b4400354
Ailesh69 <aileshsharma354@gmail.com>
2026-09-17 13:57:21 +0530

    fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-set -e 
+set -e
+alembic stamp head
 alembic upgrade head
-exec uvicorn main:app --host 0.0.0.0 --port ${PORT:8000}
+exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
